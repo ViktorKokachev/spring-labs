a4d9d2fd11e2c8da0033972a1787b762afa602ad
Victor Kokachev <victor_kokachev@epam.com>
2017-10-18 19:33:50 +0300

    lab-3: almost done

---
+++ src/test/java/lab/model/HelloWorldTest.java
@@ -0,0 +1,52 @@
+package lab.model;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class HelloWorldTest {
+
+	protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "classpath:application-context.xml";
+
+	private UsualPerson expectedPerson;
+
+	private AbstractApplicationContext context;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+		expectedPerson = getExpectedPerson();
+	}
+
+	@Test
+	public void testInitPerson() {
+		UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
+		assertThat(expectedPerson, is(person));
+		System.out.println(person);
+	}
+
+	private UsualPerson getExpectedPerson() {
+		UsualPerson person = new UsualPerson();
+		person.setAge(35);
+		person.setName("John Smith");
+
+		Country country = new Country();
+		country.setId(1);
+		country.setName("Russia");
+		country.setCodeName("RU");
+
+		person.setCountry(country);
+
+		return person;
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception{
+		if (context != null)
+			context.close();
+	}
+}
